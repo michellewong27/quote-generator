887b6eb3500ab67fa2c5e002e5c910388002c975
Michelle Wong <michellekwong2@gmail.com>
2020-03-31 14:19:30 -0400

    Added Quotes Array

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const quotes = {
-
-}
+const quotes = [
+    {
+        name: 'Stephen King',
+        quote: 'Get busy living or get busy dying.'
+    },
+    {
+        name: 'John F. Kennedy',
+        quote: 'Those who dare to fail miserably can achieve greatly.'
+    },
+    {
+        name: 'Abraham Lincoln',
+        quote: "I am a success today because I had a friend who believed in me and I didn't have the heart to let him down."
+    },
+    {
+        name: 'Leonardo Da Vinci',
+        quote: 'It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened to things.'
+    },
+    {
+        name: 'Leo Tolstoy',
+        quote: 'If you want to be happy, be.'
+    }
+]
